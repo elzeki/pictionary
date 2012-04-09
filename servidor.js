5001c187bb77dca01db0d36fcd782e2ccc77222b
ezequielglobant <ezequielglobant@globant.com>
2012-04-09 10:28:57 -0300

    bug usuario vacia fixed

--- servidor.js
+++ servidor.js
@@ -1,7 +1,7 @@
 //mejorando.la/node.js/servidor.js
 var cvanderito = require("socket.io").listen(6969);
 var lusuarios=[];
-var diccionario;
+var diccionario=[];
 var indicediccionario=0;
 var timer;
 cvanderito.sockets.on("connection", arranque);
@@ -20,7 +20,7 @@ function arranque(usuario)
 {	usuario.on("nuevoTexto", sumarpuntos);
 	usuario.on("nuevoTexto", emitir);
 	usuario.on("nuevoUsuario",cargaruser);
-  cargardiccionario();
+    cargardiccionario();
 	mostrarpalabra();
 }
 /* ------------------------------------------------------------------------*/
@@ -33,16 +33,14 @@ function asignarpuntosusuario(nombreusuario)
    var tope =  lusuarios.length;
    var i=0;
    var seguir= true;
-   console.log(tope);
-   
     while (i< tope && seguir)
-     { //console.log("user=>"+lusuarios[i]["0"]);
+     { 
       if (lusuarios[i]["0"]==nombreusuario)
-      {  console.log("si");
+      {  
         ++lusuarios[i]["1"];
         seguir=false; 
       }
-      else { ++i; console.log("noo");}
+      else {++i;}
     }
 }
 /* ------------------------------------------------------------------------*/
@@ -51,9 +49,7 @@ function  sumarpuntos(data)
      textousuario = data["1"];
 
      if (textousuario  == diccionario[indicediccionario] )
-     {  console.log("nombre:"+nombreusuario+"-"+"texto:"+textousuario);
-        asignarpuntosusuario (nombreusuario);
-        //lusuarios[0]["1"]=lusuarios[0]["1"] + 1;
+     {  asignarpuntosusuario (nombreusuario);
         reiniciartimer();
         cambiarpalabra();
         mostrarpalabra();
@@ -78,7 +74,6 @@ function seteartiempo()
 function cambiarpalabra()
 {
   var random = Math.floor (diccionario.length * Math.random ());
-  //console.log(random);
   indicediccionario=random;
 }
 /*------------------------------------------------------------------------------- */
